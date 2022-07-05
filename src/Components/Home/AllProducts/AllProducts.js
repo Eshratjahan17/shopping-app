0a695f3d2d2ddfbed48225498163ecf1e4c34635
Eshratjahan17 <eshratjahan701@gmail.com>
2022-07-06 01:10:53 +0600

    added all catagory

--- src/Components/Home/AllProducts/AllProducts.js
+++ src/Components/Home/AllProducts/AllProducts.js
@@ -1,12 +1,37 @@
+import macbook from '../../../images/products/macbook.jpg';
 import './AllProducts.css';
 const AllProducts = () => {
   return (
-    <div>
+    <div className="all-product">
       <header>
         <h1>All Catagory</h1>
       </header>
-      <div>
-
+      <div className="products">
+        <div className="product">
+          <img className="product-img" src={macbook} alt="" />
+          <h3 className="product-name">Mac Book</h3>
+          <p className="product-price">$59</p>
+        </div>
+        <div className="product">
+          <img className="product-img" src={macbook} alt="" />
+          <h3 className="product-name">Mac Book</h3>
+          <p className="product-price">$59</p>
+        </div>
+        <div className="product">
+          <img className="product-img" src={macbook} alt="" />
+          <h3 className="product-name">Mac Book</h3>
+          <p className="product-price">$59</p>
+        </div>
+        <div className="product">
+          <img className="product-img" src={macbook} alt="" />
+          <h3 className="product-name">Mac Book</h3>
+          <p className="product-price">$59</p>
+        </div>
+        <div className="product">
+          <img className="product-img" src={macbook} alt="" />
+          <h3 className="product-name">Mac Book</h3>
+          <p className="product-price">$59</p>
+        </div>
       </div>
     </div>
   );
